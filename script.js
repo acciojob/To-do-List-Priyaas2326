4a2274ad1c89d574ca5f4c5f87118d58e4fab832
Priya Sharma <113659062+Priyaas2326@users.noreply.github.com>
2023-02-11 21:35:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
+const addTodoBtn = document.getElementById("addTodoBtn");
+const newTodoInput = document.getElementById("newTodoInput");
+const todoList = document.getElementById("todoList");
+
+addTodoBtn.addEventListener("click", () => {
+  const todo = newTodoInput.value;
+
+  if (todo === "") {
+    alert("Todo item cannot be empty.");
+    return;
+  }
+
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todo;
+  todoList.appendChild(newTodo);
+
+  newTodoInput.value = "";
+});
 
